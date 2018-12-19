4767aa41d9e0d7781264b14ed5219b8e78167f65
Hector Contreras <hectorjcr@gmail.com>
2018-12-19 17:47:41 -0300

    @EnableAsync y configurado el TaskExecutor para el método asincrono del job

--- src/main/java/com/hc/jobs/JobsApplication.java
+++ src/main/java/com/hc/jobs/JobsApplication.java
@@ -4,13 +4,28 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class JobsApplication {
     
     private static Logger ROOT_LOGGER = Logger.getLogger(JobsApplication.class.getName());
+
+    @Bean ("threadPoolTaskExecutor")
+    public TaskExecutor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(20);
+        executor.setMaxPoolSize(1000);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setThreadNamePrefix("Async-");
+        return executor;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JobsApplication.class, args);
